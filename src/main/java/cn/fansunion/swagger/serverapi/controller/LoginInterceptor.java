e58ff533dafaa757dfa3d791f3bceeac56e74599
hzlimao <hzlimao@corp.netease.com>
2016-12-02 13:41:40 +0800

    no message

---
+++ src/main/java/cn/fansunion/swagger/serverapi/controller/LoginInterceptor.java
@@ -0,0 +1,42 @@
+//package cn.fansunion.swagger.serverapi.controller;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.web.method.HandlerMethod;
+//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+//
+///**
+// * @author lm
+// */
+////测试用的
+//public class LoginInterceptor extends HandlerInterceptorAdapter {
+//
+//	@Override
+//	public boolean preHandle(HttpServletRequest request,
+//			HttpServletResponse response, Object handler) throws Exception {
+//		if (!(handler instanceof HandlerMethod)) {
+//			return true;
+//		}
+//		// String str=charReader(request);
+//		// System.out.println(str);
+//		String token = request.getParameter("token");
+//		System.out.println("request.getParameter(\"token\")=" + token);
+//		return super.preHandle(request, response, handler);
+//	}
+//
+//	String charReader(HttpServletRequest request) throws IOException {
+//
+//		BufferedReader br = request.getReader();
+//
+//		String str, wholeStr = "";
+//		while ((str = br.readLine()) != null) {
+//			wholeStr += str;
+//		}
+//		return wholeStr;
+//
+//	}
+//}
---
+++ src/main/java/cn/fansunion/swagger/serverapi/controller/UserController.java
@@ -0,0 +1,141 @@
+package cn.fansunion.swagger.serverapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Lists;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+/**
+ * @author lm
+ */
+@Api(value = "user", description = "用户管理", produces = MediaType.APPLICATION_JSON_VALUE)
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+	// 列出某个类目的所有规格
+	@ApiOperation(value = "获得用户列表by_param", notes = "列表信息by_param", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "list", method = RequestMethod.POST)
+	public Result<User> list(
+			@ApiParam(value = "分类ID", required = true) @RequestParam Long categoryId,
+			@ApiParam(value = "分类ID", required = true) @RequestParam Long categoryId2,
+			@ApiParam(value = "token", required = true) @RequestParam String token) {
+		Result<User> result = new Result<User>();
+		User user = new User();
+		System.out.println(categoryId+"-->"+categoryId2);
+		
+		result.setData(user);
+		return result;
+	}
+	
+	@ApiOperation(value = "获得用户列表by_vo", notes = "列表信息by_vo", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "list_vo", method = RequestMethod.POST)
+	public Result<User> list_vo(
+			@RequestBody User user) {
+		Result<User> result = new Result<User>();
+		System.out.println(user.getName()+"<--user-->"+user.getPassword());
+		result.setData(user);
+		return result;
+	}
+	
+	@ApiOperation(value = "获得用户列表by_string_json", notes = "列表信息by_string_json", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "list_string_json", method = RequestMethod.POST)
+	public Result<User> list_string_json(
+			@ApiParam(value = "分类ID", required = true) @RequestParam String json) {
+		Result<User> result = new Result<User>();
+		System.out.println(json);
+		return result;
+	}
+	
+	@RequestMapping(value="/name/{name}", method = RequestMethod.GET)
+	@ResponseBody
+	@ApiOperation(value = "根据用户名获取list_string_variable", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE, notes = "根据用户名获取用户对象")
+	public Result<User> list_string_variable(@ApiParam(required = true, name = "name", value = "用户名") @PathVariable String name) throws Exception{
+		Result<User> result = new Result<User>();
+		System.out.println(name);
+		return result;
+	}
+
+	@ApiOperation(value = "添加用户_return_string", notes = "获取商品信息(测试返回的string)", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "add_return_string", method = RequestMethod.POST)
+	// @RequestBody只能有1个
+	// 使用了@RequestBody，不能在拦截器中，获得流中的数据，再json转换，拦截器中，也不清楚数据的类型，无法转换成java对象
+	// 只能手动调用方法
+	public String add_return_string(@RequestBody User user) {
+		String u = findUser(user);
+		System.out.println("name:"+user.getName());
+		return user.getName();
+	}
+	
+	
+	@ApiOperation(value = "添加用户_return_vo", notes = "获取商品信息(测试返回的vo)", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "add_return_vo", method = RequestMethod.POST)
+	public User add_return_vo() {
+		User user = new User();
+		user.setName("vo");
+		user.setPassword("password_vo");
+		return user;
+	}
+	
+	@ApiOperation(value = "添加用户_return_list_object", notes = "获取商品信息(测试返回的list_object)", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "add_return_list_object", method = RequestMethod.POST)
+	public List<User> add_return__list_object(@RequestBody User user) {
+		List<User> listUser = Lists.newArrayList();
+		User user1 = new User();
+		user1.setName("vo1");
+		user1.setPassword("password_vo2");
+		User user2 = new User();
+		user2.setName("vo2");
+		user2.setPassword("password_vo22");
+		listUser.add(user1);
+		listUser.add(user2);
+		return listUser;
+	}
+	
+	
+	
+
+	@ApiOperation(value = "update用户", notes = "获取商品信息(用于数据同步)", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public Result<String> update(User user) {
+		String u = findUser(user);
+		System.out.println(u);
+		return null;
+	}
+
+	@ApiOperation(value = "获得用户列表", notes = "列表信息", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@RequestMapping(value = "list_test", method = RequestMethod.POST)
+	public Result<User> list_test(
+			@ApiParam(value = "分类ID", required = true) @RequestParam Long categoryId,
+			@ApiParam(value = "分类ID", required = true) @RequestParam Long categoryId2,
+			@ApiParam(value = "token", required = true) @RequestParam String token) {
+		Result<User> result = new Result<User>();
+		User user = new User();
+		result.setData(user);
+		return result;
+	}
+	
+	private String findUser(User user) {
+		String token = user.getToken();
+		return token;
+	}
+}
